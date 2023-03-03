3a40885ece903cc177c25363640cda8e48963ff9
Khalid Imran <ekhalid090@gmail.com>
2023-03-03 07:07:31 +0600

    fixed relative class

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -24,7 +24,7 @@ export default function TodoList({todo}) {
              className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0"
         >
             <div
-                className={`rounded-full bg-white border-2  w-5 h-5 flex flex-shrink-0 justify-center
+                className={`relative rounded-full bg-white border-2  w-5 h-5 flex flex-shrink-0 justify-center
                 items-center mr-2 ${!todo.complete && 'border-gray-400'} ${todo.complete && 'border-green-500 focus-within:border-green-500'}`}
             >
                 <input
